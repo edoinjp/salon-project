ab029a2498119cf50f398a944816807fab027596
eduard gurchiani <edoinjp@gmail.com>
2024-03-23 18:21:14 +0900

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_meny :bookings
+  validates :user_name, presence: true, length:  { maximum: 50 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password_hash, presence: true
+  validates :phone_number, length: { maximum: 11 }
+
 end
