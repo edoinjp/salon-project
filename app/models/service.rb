9f0bbcef746af2b671b6cbd57a5c07bf624e1000
eduard gurchiani <edoinjp@gmail.com>
2024-03-23 18:24:22 +0900

    add serces validation

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,7 @@
 class Service < ApplicationRecord
   has_many :bookings
+
+  validates :name, presence: true
+  validates :duration, presence: ture, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
