bcdbf3b686e1e6659dfa6d43791f632951a97054
eduard gurchiani <edoinjp@gmail.com>
2024-03-28 20:03:19 +0900

    make work service creation services id show page services index page

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,6 @@ class Service < ApplicationRecord
   has_many :bookings
 
   validates :name, presence: true
-  validates :duration, presence: ture, numericality: { greater_than: 0 }
+  validates :duration, presence: true, numericality: { greater_than: 0 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
