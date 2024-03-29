bd5f7763924bee493b99ec77781c6fe7c4abd3cc
eduard gurchiani <edoinjp@gmail.com>
2024-03-29 22:17:19 +0900

    add gemfile for device#

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ ruby "3.1.2"
 gem 'chartkick'
 gem 'mini_racer'
 gem 'execjs'
+# device gem for registrations
+gem 'devise'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.2"
--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,8 +1,9 @@
-class Administrator < ApplicationRecord
+class User < ApplicationRecord
   has_many :bookings
 
   validates :username, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password_hash, presence: true
+  validates :password, presence: true, length: { minimum: 6 } #  minimum password length validation
 
+  validates :phone_number, length: { maximum: 11 }
 end
