88f20a520e80df38a444ec9bd9924c05ba06ea3d
eduard gurchiani <edoinjp@gmail.com>
2024-04-01 16:59:15 +0900

    # ApplicationController: This class serves as the base controller for your Rails application, # providing common functionality and configurations across all controllers.
    # before_action: This callback method ensures that the configure_permitted_parameters method
    # is called before any action in Devise controllers.
    
    # protected: Marks the following method as protected, ensuring that it can only be called
    # from within the class or its subclasses.
    
    # configure_permitted_parameters: This method configures permitted parameters for Devise controllers,
    # specifically allowing the 'user_name' parameter during sign-up.
    
    # devise_parameter_sanitizer: A method provided by Devise for sanitizing and permitting parameters.
    # Here, it is used to permit the 'user_name' parameter during sign-up.
    
    # permit(:sign_up, keys: [:user_name]): Permits the 'user_name' parameter during the sign-up process
    # in Devise controllers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
+# Define ApplicationController which is the base controller for your Rails application.
 class ApplicationController < ActionController::Base
+  # This before_action will call the configure_permitted_parameters method before any action in a Devise controller.
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Define a method to configure permitted parameters for Devise controllers.
+  def configure_permitted_parameters
+    # Use the devise_parameter_sanitizer to permit the 'user_name' parameter during sign-up.
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+  end
 end
