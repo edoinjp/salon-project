c8d14a762c804087dc396a0fe842eb3ae8fee286
eduard gurchiani <edoinjp@gmail.com>
2024-04-02 16:41:59 +0900

    bookings controller updated with necesery stuff

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,42 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = current_user.bookings
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = current_user.bookings.Build(booking_params)
+    if @booking.save
+      redirect_to @booking, notice:`booking was succesfully created`
+    else
+      render :new
+    end
   end
 
   def edit
+    @booking = Booking.find(params[:id])
+
   end
 
   def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+    redirect_to @booking, notice: `booking was succesfully updated`
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_url, notice: `booking was succesfully deleted`
   end
 end
