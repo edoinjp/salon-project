450a36390234f01c279cc2ba0dee22715f67f7a2
eduard gurchiani <edoinjp@gmail.com>
2024-04-02 17:10:49 +0900

    remove password_hash from administrator.rb

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -7,6 +7,4 @@ class Administrator < ApplicationRecord
 
   validates :username, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password_hash, presence: true
-
 end
